8b8e5da1ea86a56deae89374f9aef787bca6d22b
wafanakha <wafanakha15@gmail.com>
2024-01-21 08:35:03 +0700

    add function to connect to db

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ import pymysql
 
 app = Flask(__name__)
 
-conn = pymysql.connect(
-    hostname="eyn.h.filess.io",
-    database="MMOPlayerDatabase_billrough",
-    port="3307",
-    username="MMOPlayerDatabase_billrough",
-    password="b8687aac2fc979066ede1e01259edcc8db5a0b83",
-    cursorclass=pymysql.cursors.DictCursor
-)
+
+def db_conn():
+    conn = None
+    try:
+        conn = pymysql.connect(
+            hostname="eyn.h.filess.io",
+            database="MMOPlayerDatabase_billrough",
+            port="3307",
+            username="MMOPlayerDatabase_billrough",
+            password="b8687aac2fc979066ede1e01259edcc8db5a0b83",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    except pymysql.Error as e:
+        print(e)
+    return conn
+
 
 cursor = conn.cursor()
